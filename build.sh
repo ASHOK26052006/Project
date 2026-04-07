7cd8cf5b1f3e9f187ed5f78370a33df610fef78d
ASHOK P <ashokpranji@gmail.com>
2026-04-07 16:02:57 +0530

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y tesseract-ocr libzbar0 libzbar-dev
+apt-get install -y tesseract-ocr libtesseract-dev libzbar0 libzbar-dev
+
+which tesseract
+
 pip install -r requirements.txt
